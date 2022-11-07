3cd189586c52e6ce1f596f89494bb348c3781d9d
Yuhimchuk Vyacheslav <hhomebb@gmail.com>
2022-11-07 21:24:48 +0200

    Add

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+(() => {
+  const menuOpenBtn = document.querySelector('[data-menu-open]');
+  const menuCloseBtn = document.querySelector('[data-menu-close]');
+
+  const mobileMenu = document.querySelector('[data-menu]');
+
+  menuOpenBtn.addEventListener('click', openModal);
+  menuCloseBtn.addEventListener('click', openModal);
+
+  function openModal() {
+    mobileMenu.classList.toggle('is-open');
+  }
+})();
